ce69b7267cf18842341c29f5407bc68a0793e161
unknown <smaity35@gmail.com>
2019-02-24 21:27:17 +0530

    blocklist-done

--- black-list.php
+++ black-list.php
@@ -1,5 +1,5 @@
 <?php include('header.php');?>
-ddddddddddddd
+
     <section class="black-list-wrap">
         <div class="container">
             <h3>Active Empoyee list</h3>
@@ -31,9 +31,93 @@ ddddddddddddd
                             <td>Fraud, impersonation</td>
                             <td><a href="" target="_black">proof.pdf</a></td>
                             <td><a href="">Black List</a></td>
+                        </tr><tr>
+                            <td>Steve</td>
+                            <td>Smith</td>
+                            <td>PANS123</td>
+                            <td>9638839090</td>
+                            <td>TCS</td>
+                            <td>Coder</td>
+                            <td>IT</td>
+                            <td>Fraud, impersonation</td>
+                            <td><a href="" target="_black">proof.pdf</a></td>
+                            <td><a href="">Black List</a></td>
+                        </tr><tr>
+                            <td>Steve</td>
+                            <td>Smith</td>
+                            <td>PANS123</td>
+                            <td>9638839090</td>
+                            <td>TCS</td>
+                            <td>Coder</td>
+                            <td>IT</td>
+                            <td>Fraud, impersonation</td>
+                            <td><a href="" target="_black">proof.pdf</a></td>
+                            <td><a href="">Black List</a></td>
+                        </tr><tr>
+                            <td>Steve</td>
+                            <td>Smith</td>
+                            <td>PANS123</td>
+                            <td>9638839090</td>
+                            <td>TCS</td>
+                            <td>Coder</td>
+                            <td>IT</td>
+                            <td>Fraud, impersonation</td>
+                            <td><a href="" target="_black">proof.pdf</a></td>
+                            <td><a href="">Black List</a></td>
+                        </tr><tr>
+                            <td>Steve</td>
+                            <td>Smith</td>
+                            <td>PANS123</td>
+                            <td>9638839090</td>
+                            <td>TCS</td>
+                            <td>Coder</td>
+                            <td>IT</td>
+                            <td>Fraud, impersonation</td>
+                            <td><a href="" target="_black">proof.pdf</a></td>
+                            <td><a href="">Black List</a></td>
+                        </tr><tr>
+                            <td>Steve</td>
+                            <td>Smith</td>
+                            <td>PANS123</td>
+                            <td>9638839090</td>
+                            <td>TCS</td>
+                            <td>Coder</td>
+                            <td>IT</td>
+                            <td>Fraud, impersonation</td>
+                            <td><a href="" target="_black">proof.pdf</a></td>
+                            <td><a href="">Black List</a></td>
+                        </tr><tr>
+                            <td>Steve</td>
+                            <td>Smith</td>
+                            <td>PANS123</td>
+                            <td>9638839090</td>
+                            <td>TCS</td>
+                            <td>Coder</td>
+                            <td>IT</td>
+                            <td>Fraud, impersonation</td>
+                            <td><a href="" target="_black">proof.pdf</a></td>
+                            <td><a href="">Black List</a></td>
+                        </tr><tr>
+                            <td>Steve</td>
+                            <td>Smith</td>
+                            <td>PANS123</td>
+                            <td>9638839090</td>
+                            <td>TCS</td>
+                            <td>Coder</td>
+                            <td>IT</td>
+                            <td>Fraud, impersonation</td>
+                            <td><a href="" target="_black">proof.pdf</a></td>
+                            <td><a href="">Black List</a></td>
                         </tr>
                     </tbody>
               </table>
+              <ul class="pagination">
+                  <li><a href="#">1</a></li>
+                  <li class="active"><a href="#">2</a></li>
+                  <li><a href="#">3</a></li>
+                  <li><a href="#">4</a></li>
+                  <li><a href="#">5</a></li>
+                </ul>
             </div>
         </div>
     </section>
